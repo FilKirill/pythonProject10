a519bfccbced762fdb64f39093632d09b82abe05
ya22g2 <f1l1ppovc@yandex.ru>
2024-02-12 17:19:14 +0300

    задача 3

--- flask/server.py
+++ flask/server.py
@@ -14,5 +14,15 @@ def training(prof):
     return render_template('training.html', title=prof)
 
 
+@app.route('/list_prof/<flag>')
+def prof(flag):
+    proflist = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                'инженер по терраформированию', 'климатолог',
+                'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                'штурман', 'пилот дронов']
+    return render_template('proflist.html', flag=flag, proflist=proflist)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

bfe168e86010a7eaa0616849eae93344b618c8e2
ya22g2 <f1l1ppovc@yandex.ru>
2024-03-04 16:59:04 +0300

    sadfas

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from flask import Flask, url_for, render_template, redirect
+from data.users import User
+from flask import Flask
+from data.login_form import LoginForm
+from flask_login import LoginManager, login_manager, login_user
+from data import db_session
+from data.jobs import Jobs
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db_session.global_init('db/mars_explorer.db')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+@app.route('/index')
+def index():
+    db_sess = db_session.create_session()
+
+    jobs = db_sess.query(Jobs).all()
+
+    return render_template('jobs.html', jobs=jobs)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(port=8080, host='127.0.0.1')
